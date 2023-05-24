95723c6f04221371d8389cbdb9e79e7aa3e95d6c
ngtinh <nguyenvanductinh77@gmail.com>
2023-05-24 13:50:24 +0700

    Sử dụng GridView cho nó đẹp hơn

--- foodapp/lib/view/Admin/HomeAdmin/HomeAdmin.dart
+++ foodapp/lib/view/Admin/HomeAdmin/HomeAdmin.dart
@@ -6,69 +6,132 @@ import 'package:foodapp/view/Admin/Manager/Users.dart';
 import 'package:foodapp/view/Admin/Manager/Revenue.dart';
 
 class HomeScreen extends StatelessWidget {
-  Future<void> _refreshData() async {
-    // Thực hiện logic làm mới dữ liệu ở đây
-    // Ví dụ: gọi API để cập nhật dữ liệu mới
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: RefreshIndicator(
-        onRefresh: _refreshData,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ManageCategory()),
-                );
-              },
-              child: Text('Quản lý danh mục món ăn'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16),
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ManageCategory()),
+              );
+            },
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  radius: 40,
+                  child: Icon(Icons.category, size: 40),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Quản lý danh mục',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ManageFood()),
-                );
-              },
-              child: Text('Quản lý món ăn'),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ManageFood()),
+              );
+            },
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.green,
+                  radius: 40,
+                  child: Icon(Icons.restaurant_menu, size: 40),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Quản lý món ăn',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ManageOrders()),
-                );
-              },
-              child: Text('Quản lý đơn đặt món ăn'),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ManageOrders()),
+              );
+            },
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.orange,
+                  radius: 40,
+                  child: Icon(Icons.assignment, size: 40),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Quản lý đơn',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ManageUsers()),
-                );
-              },
-              child: Text('Quản lý người dùng'),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ManageUsers()),
+              );
+            },
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.purple,
+                  radius: 40,
+                  child: Icon(Icons.people, size: 40),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Quản lý người dùng',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Revenue()),
-                );
-              },
-              child: Text('Doanh thu'),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Revenue()),
+              );
+            },
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.red,
+                  radius: 40,
+                  child: Icon(Icons.bar_chart, size: 40),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Quản lý doanh thu',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
