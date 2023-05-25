f3777042761015e6be78be6d5e1907efdc814207
ngtinh <nguyenvanductinh77@gmail.com>
2023-05-25 23:11:04 +0700

    Chọn vị trí trên map

--- foodapp/lib/view/User/MapScreen.dart
+++ foodapp/lib/view/User/MapScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -8,6 +10,48 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   GoogleMapController? mapController;
+  String? selectedAddress;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  void getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    moveToLocation(position.latitude, position.longitude);
+  }
+
+  void moveToLocation(double latitude, double longitude) {
+    mapController?.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(latitude, longitude),
+          zoom: 14.0,
+        ),
+      ),
+    );
+  }
+
+  Future<void> searchAndSelectAddress(LatLng latLng) async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      latLng.latitude,
+      latLng.longitude,
+    );
+
+    if (placemarks.isNotEmpty) {
+      Placemark placemark = placemarks.first;
+      String address =
+          '${placemark.street}, ${placemark.subLocality}, ${placemark.subAdministrativeArea}';
+
+      setState(() {
+        selectedAddress = address;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +69,15 @@ class _MapScreenState extends State<MapScreen> {
           target: LatLng(10.78722, 106.6537159),
           zoom: 14.0,
         ),
+        onTap: (LatLng latLng) {
+          searchAndSelectAddress(latLng);
+        },
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          // Gán vị trí là ...
-          String selectedAddress = '16 Chử Đồng Tử, Phường 7, Quận Tân Bình';
-
-          Navigator.pop(context, selectedAddress);
+          if (selectedAddress != null) {
+            Navigator.pop(context, selectedAddress);
+          }
         },
         label: Text('Chọn địa chỉ'),
         icon: Icon(Icons.place),
