aaa8e403cc77d97681b4e452b3609ebf6c61151d
ngtinh <nguyenvanductinh77@gmail.com>
2023-05-26 01:16:57 +0700

    Chỉnh Ui info user

--- foodapp/lib/view/User/Infomation.dart
+++ foodapp/lib/view/User/Infomation.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:foodapp/config/apihelper.dart';
 import 'package:foodapp/config/pref.dart';
-import 'package:foodapp/view/Home/home.dart';
 import 'package:foodapp/view/Login/Login.dart';
-import 'package:foodapp/view/Product/productsearch.dart';
 import 'package:foodapp/view/User/Changinfo.dart';
-import 'package:foodapp/view/order/cart.dart';
-import 'package:foodapp/view/order/listorder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Information extends StatefulWidget {
@@ -65,301 +61,190 @@ class _InformationState extends State<Information> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Column(
-                  children: [
-                    Center(child: Text("Xin chào !")),
-                    Text(name),
-                    if (image != "")
-                      InkWell(
-                        child: CircleAvatar(
-                          child: Image.network(
-                              "${Apihelper.image_base}/avatar/${image}"),
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Information()));
-                        },
-                      )
-                  ],
-                )),
-            ListTile(
-              title: Text("TRANG CHỦ"),
-              onTap: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => Home()));
-              },
-            ),
-            ListTile(
-              title: Text("GIỎ HÀNG"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => CartList()));
-              },
-            ),
-            ListTile(
-              title: Text("TẤT CẢ ĐƠN HÀNG"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ListOrder("4")));
-              },
-            ),
-            ListTile(
-              title: Text("ĐANG XỬ LÝ"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ListOrder("0")));
-              },
-            ),
-            ListTile(
-              title: Text("ĐANG GIAO HÀNG"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ListOrder("1")));
-              },
-            ),
-            ListTile(
-              title: Text("THÀNH CÔNG"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ListOrder("2")));
-              },
-            ),
-            ListTile(
-              title: Text("ĐÃ HỦY"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ListOrder("3")));
-              },
-            ),
-            if (login)
-              ListTile(
-                title: Text("ĐĂNG XUẤT"),
-                onTap: () {
-                  logout();
-                },
-              )
-            else
-              ListTile(
-                title: Text("ĐĂNG Nhập"),
-                onTap: () {
-                  logout();
-                },
-              ),
-          ],
-        ),
-      ),
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         title: Row(
           children: [
-            Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: InkWell(
-                  child: Icon(Icons.arrow_back_ios, color: Colors.pinkAccent),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                )),
-            Container(
-                padding: EdgeInsets.only(top: 5, left: 20),
-                height: 40,
-                width: 300,
-                // margin: EdgeInsets.only(),
-                child: Stack(
-                  alignment: Alignment.centerRight,
-                  children: [
-                    TextFormField(
-                      controller: search,
-                      textInputAction: TextInputAction.search,
-                      //   onTap: (){
-                      //
-                      // },
-                      onEditingComplete: () {
-                        if (search.text != "")
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      SearchProduct(search.text)));
-                      },
-                      decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.only(bottom: 3, left: 13, right: 30),
-                        hintText: "Tìm kiếm",
-                        hintStyle: TextStyle(
-                            color: Colors.grey,
-                            fontFamily: 'Roboto',
-                            fontSize: 13),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: BorderSide(),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              color: Color.fromRGBO(59, 185, 52, 1),
-                              width: 1.0),
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 10.0),
-                      child: InkWell(
-                        child: Icon(
-                          Icons.dangerous_rounded,
-                          color: Colors.pinkAccent,
-                        ),
-                        onTap: () {
-                          search.text = "";
-                        },
-                      ),
-                    ),
-                  ],
-                )),
-            Padding(
-              padding: EdgeInsets.only(left: 10, top: 10),
-              child: InkWell(
-                child: Icon(
-                  Icons.shopping_cart,
-                  color: Colors.pinkAccent,
-                ),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => CartList()));
-                },
-              ),
-            ),
+            Text("Thông tin"),
           ],
         ),
-
-        backgroundColor: Colors.white,
-        // leading: IconButton(icon:Icon( Icons.arrow_back_ios,color: Color.fromRGBO(59, 185, 52, 1),),onPressed: (){
-        //   Navigator.pop(context);
-        // },),
+        backgroundColor: Colors.blue,
       ),
       body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          SizedBox(height: 40),
           Center(
             child: Container(
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(50))),
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.blue, width: 2),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
               width: 150,
-              child: Image.network(
-                "${Apihelper.image_base}/avatar/$image",
-                width: 250,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Container(
-            width: 440,
-            padding: EdgeInsets.only(left: 50),
-            child: Row(
-              children: [
-                Container(
-                  width: 150,
-                  child: Text(
-                    "Họ tên:",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    "$name",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            width: 440,
-            padding: EdgeInsets.only(left: 50),
-            child: Row(
-              children: [
-                Container(
-                  width: 150,
-                  child: Text(
-                    "Số điện thoại",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    (phone != null) ? "$phone" : "",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            width: 440,
-            padding: EdgeInsets.only(left: 50),
-            child: Row(children: [
-              Container(
-                width: 150,
-                child: Text(
-                  "Email",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              height: 150,
+              child: ClipOval(
+                child: Image.network(
+                  "${Apihelper.image_base}/avatar/$image",
+                  fit: BoxFit.cover,
                 ),
               ),
-              Container(
-                child: Text(
-                  (email != null) ? "$email" : "",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ]),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            width: 440,
-            padding: EdgeInsets.only(left: 50),
-            child: Row(
-              children: [
-                Container(
-                  width: 150,
-                  child: Text(
-                    "Địa chỉ",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    (address != null) ? "$address" : "",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
             ),
           ),
-          TextButton(
+          SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "Họ tên:",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      offset: Offset(0, 2),
+                    ),
+                  ]),
+            ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "$name",
+              style: TextStyle(fontSize: 20, shadows: [
+                Shadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  offset: Offset(0, 2),
+                ),
+              ]),
+            ),
+          ),
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "Số điện thoại:",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      offset: Offset(0, 2),
+                    ),
+                  ]),
+            ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              (phone != null) ? "$phone" : "",
+              style: TextStyle(fontSize: 20, shadows: [
+                Shadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  offset: Offset(0, 2),
+                ),
+              ]),
+            ),
+          ),
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "Email:",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      offset: Offset(0, 2),
+                    ),
+                  ]),
+            ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              (email != null) ? "$email" : "",
+              style: TextStyle(fontSize: 20, shadows: [
+                Shadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  offset: Offset(0, 2),
+                ),
+              ]),
+            ),
+          ),
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "Địa chỉ:",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      offset: Offset(0, 2),
+                    ),
+                  ]),
+            ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              (address != null) ? "$address" : "",
+              style: TextStyle(fontSize: 20, shadows: [
+                Shadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  offset: Offset(0, 2),
+                ),
+              ]),
+            ),
+          ),
+          SizedBox(height: 40),
+          Center(
+            child: TextButton(
               onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Changinfo()));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Changinfo(),
+                  ),
+                );
               },
-              child: Text("CẬP NHẬT THÔNG TIN"))
+              child: Text(
+                "CẬP NHẬT THÔNG TIN",
+                style: TextStyle(fontSize: 18, shadows: [
+                  Shadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    offset: Offset(0, 2),
+                  ),
+                ]),
+              ),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                shadowColor: Colors.grey.withOpacity(0.5),
+                elevation: 5,
+              ),
+            ),
+          ),
         ],
       ),
     );
