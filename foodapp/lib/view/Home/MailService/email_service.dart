a893d996e7d018823bbd1b501cad15de2c14824e
ngtinh <nguyenvanductinh77@gmail.com>
2023-05-28 21:59:19 +0700

    Gửi mail đặt món ăn thành công

--- foodapp/lib/view/Home/MailService/email_service.dart
+++ foodapp/lib/view/Home/MailService/email_service.dart
@@ -2,14 +2,31 @@ import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
 
 class EmailService {
+  static final SmtpServer smtpServer =
+      gmail('contact.tripletfood@gmail.com', 'zfuyqewxzdhedgol');
+
   static Future<void> sendRegistrationEmail(String recipientEmail) async {
-    final smtpServer =
-        gmail('contact.tripletfood@gmail.com', 'zfuyqewxzdhedgol');
     final message = Message()
       ..from = Address('contact.tripletfood@gmail.com')
       ..recipients.add(recipientEmail)
       ..subject = 'Tạo tài khoản thành công'
       ..text = 'Bạn đã tạo tài khoản thành công';
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Email sent: ${sendReport.toString()}');
+    } catch (e) {
+      print('Error sending email: $e');
+    }
+  }
+
+  static Future<void> sendOrderInfoEmail(
+      String recipientEmail, String orderDetails) async {
+    final message = Message()
+      ..from = Address('contact.tripletfood@gmail.com')
+      ..recipients.add(recipientEmail)
+      ..subject = 'Đặt món ăn thành công'
+      ..text = '$orderDetails';
 
     try {
       final sendReport = await send(message, smtpServer);
