a28b123b7b8f94c1c8cf6a0cf60113e7b32d778e
ngtinh <nguyenvanductinh77@gmail.com>
2023-05-29 23:42:52 +0700

    Tạo be khuyến mãi

---
+++ be_foodapp/database/migrations/2023_05_29_220451_create_coupons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('code', 50);
+            $table->integer('count');
+            $table->integer('promotion');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
