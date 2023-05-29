125b7a5a1c4ee53bb93a8b3cf8a1cc68fa800d99
ngtinh <nguyenvanductinh77@gmail.com>
2023-05-29 14:38:03 +0700

    Chinh Ui tí nữa

--- foodapp/lib/view/Login/Login.dart
+++ foodapp/lib/view/Login/Login.dart
@@ -5,7 +5,6 @@ import 'package:foodapp/config/apihelper.dart';
 import 'package:foodapp/view/Admin/HomeAdmin/MainAdmin.dart';
 import 'package:foodapp/view/Home/home.dart';
 import 'package:foodapp/view/Login/Forget.dart';
-import 'package:foodapp/view/Login/Register.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
@@ -135,18 +134,6 @@ class _Login extends State<Login> {
                               backgroundColor: Color.fromRGBO(59, 185, 52, 1),
                               shape: StadiumBorder())),
                     ),
-                    TextButton(
-                        child: Text("Đăng kí"),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Register()));
-                        },
-                        style: ButtonStyle(
-                          foregroundColor: MaterialStateProperty.all<Color>(
-                              Colors.blueAccent),
-                        )),
                     TextButton(
                         child: Text("Quên mật khẩu"),
                         onPressed: () {
